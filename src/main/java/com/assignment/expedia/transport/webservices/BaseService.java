12271e7aa02021bf7df4c7a06f6214b19787a427
sandeepandey <sandeep.masum4685@gmail.com>
2014-12-14 19:51:32 +0530

    Commiting Base Service

--- src/main/java/com/assignment/expedia/transport/webservices/BaseService.java
+++ src/main/java/com/assignment/expedia/transport/webservices/BaseService.java
@@ -5,10 +5,7 @@ import com.assignment.expedia.model.HotelDeal;
 import com.assignment.expedia.utils.json.JSONHelper;
 import com.assignment.expedia.utils.misc.Pair;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -22,10 +19,10 @@ import java.util.List;
 public class BaseService {
 
     @GET
-    @Path("/processRequest/{paramName}/{paramValue}")
+    @Path("/processRequest")
     @Produces(MediaType.APPLICATION_JSON)
-	public Response processRequest(@PathParam("paramName") String p_param_name,
-			@PathParam("paramValue") String p_param_value){
+	public Response processRequest(@QueryParam("paramName") String p_param_name,
+			@QueryParam("paramValue") String p_param_value){
 		System.out.println("[Collected Input Param are .]"+Pair.makePair(p_param_name, p_param_value));
 		List<HotelDeal> l_result =DealStorageHelper.getResults(p_param_name, p_param_value.toLowerCase());
 		return Response.ok(JSONHelper.convertObjectToJSONString(l_result)).build();
